6cd3730b4ea46331f1a7eb290255dc7ccf4a8d21
Alex Nascimento <alexjose68@hotmail.com>
2024-07-27 14:06:17 -0300

    feat: na página [id] coloquei ela para ser client side, para poder utilizar o useState e useEffect do react e pegar os dados da api através do retorno da nossa função getData, e passamos este resultado numa tag pre.

--- src/app/(site)/repositorios/[id]/page.tsx
+++ src/app/(site)/repositorios/[id]/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+import { useState, useEffect } from 'react';
 interface PageDetailProps {
   params: {
     id: string;
@@ -12,11 +14,26 @@ async function getData(id: string) {
 }
 
 export default async function RepositorioId({ params } : PageDetailProps) {
-  const data = await getData(params.id);
+  const [data, setData] = useState<any>(null);
+  // const data = await getData(params.id);
+
+  useEffect(() => {
+    async function fetchData() {
+      const result = await getData(params.id);
+      setData(result);
+    }
+
+    fetchData();
+  }, [params.id]);
+
+  if (data === null) {
+    return <div><h2>Loading...</h2><p>{String(data)}</p></div>
+  }
 
   return (
     <div>
       <h1>Página detalhes do repositório {params.id}</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   )
 }
